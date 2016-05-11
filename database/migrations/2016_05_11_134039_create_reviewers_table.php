a3ab74f7aaee2c0ee2598c1a554b2792c1022af3
Slawek <s.pelka@live.nl>
2016-05-11 15:44:58 +0200

    added reviewer table

---
+++ database/migrations/2016_05_11_134039_create_reviewers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviewers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('company_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->integer('telephone_number');
+            $table->string('company');
+            $table->string('function');
+            $table->string('location');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reviewers');
+    }
+}
