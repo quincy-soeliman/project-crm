4bec3eb7e58f337ea437496c44fe1e2e0e388dd7
Slawek <s.pelka@live.nl>
2016-05-12 11:58:02 +0200

    added administrator migration and model

---
+++ database/migrations/2016_05_12_095502_create_administrators_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdministratorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrators', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->integer('telephone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('administrators');
+    }
+}
