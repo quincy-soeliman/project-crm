bbf829116c6b85e0bcdd79ddb1084c9bd9d7f82a
Slawek <s.pelka@live.nl>
2016-05-23 14:02:14 +0200

    added login screen style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="background">
+    <div class="color-overlay"></div>
+    <div class="background-image"></div>
+</div>
+<div class="container login-container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="login-screen col-md-4 col-md-offset-4 col-xs-12 col-xs-push-0">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group">
+                            <div class="col-md-12 col-xs-12">
+                                <label for="role-select">Login als:</label>
+                                <select name="role-select" id="role-select" class="role-select col-md-10-col-xs-10">
+                                    <option value="student">Student</option>
+                                    <option value="docent">Docent</option>
+                                    <option value="beoorderlaar">Beoordelaar</option>
+                                    <option value="bedrijf">Bedrijf</option>
+                                </select>
+                            </div>
+                        </div>
 
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <div class="col-md-12 col-xs-12">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -25,10 +38,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
+                            <div class="col-md-12">
+                                <input type="password" class="form-control" name="password" placeholder="Password">
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -39,22 +51,19 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
+                            <div class="col-md-12 col-xs-12">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-sign-in"></i>Login
+                                </button>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
-                                </button>
-
+                            <div class="col-md-6 col-xs-12">
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                            </div>
+                            <div class="col-md-6 col-xs-12">
+                                <a class="btn btn-link" href="{{ url('/register') }}">Register</a>
                             </div>
                         </div>
                     </form>
