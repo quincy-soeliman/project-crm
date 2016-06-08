0934a4c41df2c7a168b777bfff04c0850332d816
Slawek <s.pelka@live.nl>
2016-06-08 13:57:58 +0200

    added fancy comments to scripts file

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,20 +1,33 @@
 function analyseDropdown() {
+    // Append toggle attribute to all dropdown buttons
     $('.trigger-dropdown').attr('toggled', 'no');
-
+    // Loop trough all triggers
     $('.trigger-dropdown').each( function() {
+        // Save selectors in variables for easier use
         var $this = $(this);
         var $testTarget = $this.attr('toslidedown');
-
+        /*
+        Check if list is empty
+        If so, hide it
+         */
         if( $this.parent().find($testTarget).length === 0 ) {
             $this.parent().addClass("hide");
         }
     });
-
+    // On trigger click fire function
     $(".trigger-dropdown").on('click', function() {
+        // Save selectors in variables for easier use
         var $trigger = $(this);
         var $target = $(this).attr('toslidedown');
         var toggleState = $(this).attr('toggled');
-
+        /*
+        If togglestate of trigger === 'no'
+        Open the dropdown
+        Change togglestate of trigger to 'yes'
+        Else
+        Close dropdown
+        Set togglestate of trigger to 'no'
+         */
         if( toggleState === 'no' ) {
             $trigger.parent().find($target).each( function() {
                 $(this).slideDown('fast', function() {
@@ -32,9 +45,14 @@ function analyseDropdown() {
 }
 
 function hasUsers() {
+    // Loop trough all user-containers 1 by 1
     $(".user-container").each( function() {
+        // Save selector in variables for easier use
         var $this = $(this);
-
+        /*
+        If can't find container with class 'user'
+        then hide the user-container
+         */
         if( $this.find('.user').length == 0 ) {
             $this.hide();
         }
@@ -42,6 +60,7 @@ function hasUsers() {
 }
 
 function initSelect2() {
+    // Simple init of select2 plugin
     $("#reviewer").select2();
     $(".analyse-workprocess-select-field").select2();
 }
@@ -50,7 +69,7 @@ $(function() {
     // Init anaylse dropdown
     analyseDropdown();
 
-    // Cheack if has users
+    // Check if has users
     hasUsers();
 
     // Init select2
